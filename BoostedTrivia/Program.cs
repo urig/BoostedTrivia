c1d60ae32989178e6a3b97430700332413fcd5de
Uri Goldstein <uri.goldstein@gmail.com>
2017-10-11 23:57:43 +0300

    Ported Runner into Program.cs

--- BoostedTrivia/Program.cs
+++ BoostedTrivia/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BoostedTrivia
 {
     class Program
     {
+        private static bool notAWinner;
+
         static void Main(string[] args)
         {
+            Game aGame = new Game(null);
+        
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+        
+            Random rand = new Random();
+    
+            do {
+            
+                aGame.roll(rand.Next(5) + 1);
+            
+                if (rand.Next(9) == 7) {
+                    notAWinner = aGame.wrongAnswer();
+                } else {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            
+            
+            
+            } while (notAWinner);
+            Console.ReadKey();
         }
     }
 }
